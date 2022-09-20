f0140c23fe91544eb6417be60a675b80a1cef56b
13761566975 <q19891025>
2022-09-20 15:37:11 +0800

    修改

--- tutorial-basic/script.js
+++ tutorial-basic/script.js
@@ -1,4 +1,4 @@
-// import { ethers } from "https://cdn-cors.ethers.io/lib/ethers-5.6.9.esm.min.js";
+// import { ethers } from "https://cdn-cors.ethers.io/lib/ethers-5.6.9.esm.min.js";//
 const ethers = require('ethers');
 
 const provider = ethers.getDefaultProvider();
